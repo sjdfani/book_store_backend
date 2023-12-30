8360bbdb55aadc95031ed0e39dadb27b914433cb
Sajjad fani <faniam321@gmail.com>
2023-12-30 22:34:44 +0330

    add new app

--- book_store_backend/book_store_backend/settings.py
+++ book_store_backend/book_store_backend/settings.py
@@ -50,7 +50,7 @@ THIRD_PARTY_APPS = [
 ]
 
 LOCAL_APPS = [
-
+    'books.apps.BooksConfig',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
--- book_store_backend/book_store_backend/urls.py
+++ book_store_backend/book_store_backend/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/books/', include('books.urls'), name='books'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
