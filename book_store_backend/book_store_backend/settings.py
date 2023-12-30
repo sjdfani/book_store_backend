793364678889968d52461d697db80d54624c58c5
Sajjad fani <faniam321@gmail.com>
2023-12-30 22:10:55 +0330

    delete extra info

--- book_store_backend/book_store_backend/settings.py
+++ book_store_backend/book_store_backend/settings.py
@@ -50,9 +50,7 @@ THIRD_PARTY_APPS = [
 ]
 
 LOCAL_APPS = [
-    'users.apps.UsersConfig',
-    'schedule.apps.ScheduleConfig',
-    'masters.apps.MastersConfig',
+
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
