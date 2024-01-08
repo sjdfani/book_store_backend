e734164f7814457a47b891da097c1bd60bd5225a
Sajjad fani <faniam321@gmail.com>
2024-01-08 22:44:49 +0330

    complete admin

--- book_store_backend/users/admin.py
+++ book_store_backend/users/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    readonly_fields = ("created_at", "updated_at")
+    list_display = (
+        "username", "is_staff", "is_active", "is_superuser")
+    list_filter = ("is_staff", "is_active", "is_superuser")
+    fieldsets = (
+        ('Personal Information', {
+         "fields": ("username", "fullname", "profile", "password")}),
+        ("Permissions", {"fields": (
+            "is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Date and Time", {
+         "fields": ("last_login", "created_at", "updated_at")}),
+    )
+    add_fieldsets = (
+        ('Personal Information', {
+            "classes": ("wide",),
+            "fields": ("username", "fullname", "profile", "password1", "password2")}),
+        ("Permissions", {"fields": (
+            "is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+    )
+    search_fields = ("username",)
+    ordering = ("username",)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
