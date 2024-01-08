9852dc60967287cfecc5d4711d4f9d52de20de3f
Sajjad fani <faniam321@gmail.com>
2024-01-08 22:48:03 +0330

    complete admin

--- book_store_backend/books/admin.py
+++ book_store_backend/books/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Book, SaveItem
 
-# Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        "title", "author", "language", "price", "publish")
+    list_filter = ("author", "language", "publish")
+    list_editable = ("publish",)
+
+
+class SaveItemAdmin(admin.ModelAdmin):
+    list_display = ("user", "book")
+    list_filter = ("user", "book")
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(SaveItem, SaveItemAdmin)
