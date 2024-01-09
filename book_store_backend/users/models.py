cdc21f9ae4d43f929b28b1ae5e11118a574e7dbd
Sajjad fani <faniam321@gmail.com>
2024-01-09 09:46:54 +0330

    add new custom model

--- book_store_backend/users/models.py
+++ book_store_backend/users/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+from users.managers import CustomUserManager
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    first_name = None
+    last_name = None
+    username = None
+    email = models.EmailField(_("Email Address"), unique=True)
+    profile = models.ImageField(
+        _("Profile"), upload_to="profile/", null=True, blank=True)
+    fullname = models.CharField(_("Fullname"), max_length=255)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["fullname"]
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+    def update_last_login(self):
+        self.last_login = timezone.now()
+        self.save()
