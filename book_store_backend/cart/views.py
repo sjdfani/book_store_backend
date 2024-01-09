5c0d55924619fcd930be41515fd6acd418f13e19
Sajjad fani <faniam321@gmail.com>
2024-01-09 10:41:53 +0330

    complete get cart

--- book_store_backend/cart/views.py
+++ book_store_backend/cart/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Cart, PurchaseItem
+from .serializers import (
+    CartSerializer,
+)
 
-# Create your views here.
+
+class GetCart(generics.ListAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = CartSerializer
+
+    def get_queryset(self):
+        return Cart.objects.filter(user=self.request.user)
