606c035f64da951cd036ad4d9e1540cd4aee9b73
Sajjad fani <faniam321@gmail.com>
2024-01-09 11:38:57 +0330

    complete admin

--- book_store_backend/cart/admin.py
+++ book_store_backend/cart/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Cart, PurchaseItem
 
-# Register your models here.
+
+class PurchaseItemsAdmin(admin.ModelAdmin):
+    list_display = ("user", "count", "created_at", "updated_at")
+
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ("user", "updated_at")
+
+
+admin.site.register(Cart, CartAdmin)
+admin.site.register(PurchaseItem, PurchaseItemsAdmin)
