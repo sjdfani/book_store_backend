20630099cd025187946506962071f60846dc96c0
Sajjad fani <faniam321@gmail.com>
2024-01-30 02:56:12 +0330

    clean code

--- book_store_backend/cart/admin.py
+++ book_store_backend/cart/admin.py
@@ -1,14 +1,9 @@
 from django.contrib import admin
-from .models import Cart, PurchaseItem
+from .models import PurchaseItem
 
 
 class PurchaseItemsAdmin(admin.ModelAdmin):
     list_display = ("user", "count", "created_at", "updated_at")
 
 
-class CartAdmin(admin.ModelAdmin):
-    list_display = ("user", "updated_at")
-
-
-admin.site.register(Cart, CartAdmin)
 admin.site.register(PurchaseItem, PurchaseItemsAdmin)
